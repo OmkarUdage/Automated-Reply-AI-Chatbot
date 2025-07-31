db7d0b77c212d0484e698350f38bac92728d7ec7
Omkar Udage <116948012+OmkarUdage@users.noreply.github.com>
2025-07-31 13:30:46 +0530

    Update client.py

--- client.py
+++ client.py
@@ -1,12 +1,10 @@
 # Cohere AI API integration 
 import cohere
 
-co = cohere.ClientV2("ryuzsDhsqGoxvlPVyC3eCrzTnpUgubMk2huH09AA")
+co = cohere.ClientV2("---")
 
 command = '''
-[07:39, 18/07/2025] Shubham Dada: https://www.instagram.com/reel/DMNzpzmJ3t1/?igsh=eDFmdnM1YWo0d2lt
-[10:33, 26/07/2025] Shubham Dada: I'm really enjoying this course on Udemy and think you might like it too.
-https://www.udemy.com/share/103BPI3@5XRebDvG6UHGqu8yCAv9IEAcj_pGDAfA6yGZea_Zf0vjVjCK0dw5u-WqV8Ny0_U3BQ==/
+# Your recent messages from the chat
 '''
 response = co.chat(
     model="command-a-03-2025", 
